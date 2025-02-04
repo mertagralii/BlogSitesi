9b8025637a7816ae1a5c88bb607bab1efea04266
Mert Ağralı <my_mert07hotmail.com>
2025-02-04 08:08:25 +0300

    LEFT JOİN Modelinde Düzenleme yapıldı, tablo isimlerinde güncellemeye gidildi.

--- Models/IndexViewModel.cs
+++ Models/IndexViewModel.cs
@@ -2,19 +2,23 @@
 {
     public class IndexViewModel
     {
-        public int Id { get; set; }
+        public int BlogId { get; set; }
         public string Title { get; set; }
         public string Summary { get; set; }
         public string Description { get; set; }
         public DateTime CreatedDate { get; set; }
+
+        public DateTime UpdateDate { get; set; }
         public bool IsDeleted { get; set; }
         public bool IsApproved { get; set; }
         public bool IsIndex { get; set; }
 
-        // Kategori bilgisi
+        // Kategori bilgisi,
+        public int CategoryId { get; set; }
         public string CategoryName { get; set; }
 
         // Yazar bilgisi
+        public int AuthorId { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
     }
