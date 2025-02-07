6f7f5abe0fb2fd66d85b6c3a8c36bfd58eb977a8
Mert Ağralı <my_mert07hotmail.com>
2025-02-07 20:50:13 +0300

    Yazarlar sayfası öncesi viewmodel tasarlandı

---
+++ Models/AuthorsViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace BlogSitesi.Models
+{
+    public class AuthorsViewModel
+    {
+        public int AuthorsId { get; set; }
+
+        public string Name { get; set; }
+
+        public string SurName { get; set; }
+
+        public int PostCount { get; set; }
+    }
+}
